de77c91b1a95c78b46e8a2f3c1dcf423510c76be
Billy Michael <billy_michael@outlook.com>
2018-05-09 14:21:51 +0100

    Added comments

--- getConflict.py
+++ getConflict.py
@@ -6,26 +6,33 @@ import logging
 import os
 from requests.exceptions import ConnectionError
 
+# Set the log stream to output via stdout
 logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
 
+#Construct the URL based on input parameters
 url = 'http://%s/api/v1/branches/%s/conflicts?applicationName=%s&applicationVersion=%s' % (sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4])
 logging.info('REST URL has been generated: %s' % url)
 
+# Perform the GET request to check for conflicts. 
 try:
     r = requests.get(url)
-except ConnectionError as e:    # This is the correct syntax
+except ConnectionError as e:
     print "Unable to connect to the specified URL. Please check the supplied parameters"
     sys.exit(1)
 
+# Check that the correct status code is returned
 if r.status_code == 500:
     logging.info('User details successfully obtained.')
 else:
-    logging.error("Unable to get user details. [HTTP response=%s]." % (r.text))
+    logging.error("Unable to check for conflicts. [HTTP response=%s]." % (r.text))
 
+# Parse the JSON
 details =  json.loads(r.text)
 
+# Retrieve the conflict count from the JSON
 conflictCount = details["conflictsCount"]
 
+# Check if there are any conflicts. If there are, fail the build
 if conflictCount == 1:
     logging.error("Conflict Detected.")
     sys.exit(1)
